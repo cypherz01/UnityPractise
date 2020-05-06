ed05fbc0a7d6d2f439efd186c78c184030c6cc77
cypherz01 <mrtomcuthbertson@gmail.com>
2020-05-06 15:44:10 +0100

    added camera follow

---
+++ Unity Practise/Assets/scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Target;
+
+    private Vector3 cameraOffset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraOffset = transform.position - Target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Target.transform.position + cameraOffset;
+    }
+}
